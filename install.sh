37251451001c7898d7c632a85f619e941a69fe07
grigoris <xargrigoris@gmail.com>
2018-01-09 20:33:26 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,29 @@
 clear
 
 sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt autoremove
+
+sudo apt-get upgrade -yqq
+
+sudo apt-get dist-upgrade -yqq
+
+sudo apt autoremove -y
+
 sudo apt autoclean
-sudo apt-get install npm
+
+sudo apt-get install npm -y
+
 npm i npm@latest -g
+
 npm -v
+
 npm i n@latest -g
+
 n lts
 
+npm i npm-check-updates@latest
+
+npm list -g --depth=0
+
+rm install.sh
+
+sudo apt-get install nginx -yqq
